b913cd9edaf1357cf37b63cc09e8ff79aa9575bf
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2021-06-12 22:29:07 +0530

    Added special case for zero.

--- fibbonacciGenerator.js
+++ fibbonacciGenerator.js
@@ -8,11 +8,11 @@ function fibonacciGenerator (n) {
         output.push(newN);
     }
     //Return an array of fibonacci numbers starting from 0.
-    if(n<=2){
-return output[n-1];
-}else{
+    if(n===1){
+return [0];
+}
+else{
 return output;
 }
 //Do NOT change any of the code below 👇
 }
-
