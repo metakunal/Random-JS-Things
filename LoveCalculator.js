9feb0373180dfbd1509bebe423669f783bc0d2b6
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2021-06-12 13:13:34 +0530

    Create LoveCalculator.js

---
+++ LoveCalculator.js
@@ -0,0 +1,6 @@
+prompt("Enter Your Name : ");
+prompt("Enter the Name of your Crush : ");
+var n=Math.random();
+n=n*100;
+n=Math.floor(n)+1;
+alert("The chances that oyur crush will be yours is "+n+" %.")
