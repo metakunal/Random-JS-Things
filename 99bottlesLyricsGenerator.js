57dd2645f0eb4a21353483fb1b7ec35d6013f9d9
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2021-06-12 17:55:16 +0530

    Create 99bottlesLyricsGenerator.js

---
+++ 99bottlesLyricsGenerator.js
@@ -0,0 +1,8 @@
+function lyrics(){
+    var i=99;
+    while(i>0){
+console.log(i+" bottles of beer on the wall, "+i+" bottles of beer.")
+console.log("Take one down and pass it around, "+(i-1)+"bottles of beer on the wall.")
+i--;
+}
+}
